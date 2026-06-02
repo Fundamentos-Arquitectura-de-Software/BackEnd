842bd93e409fefd647a4d664881fb0b504ca4af2
Camila <u202110385@upc.edu.pe>
2026-06-01 19:42:54 -0500

    feat(inventory): add validations for the bounded context Inventory.

--- src/main/java/com/acme/backendfreshsense/inventory/application/dto/ProductRequest.java
+++ src/main/java/com/acme/backendfreshsense/inventory/application/dto/ProductRequest.java
@@ -1,13 +1,35 @@
 package com.acme.backendfreshsense.inventory.application.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
+import jakarta.validation.constraints.*;
 
 public record ProductRequest(
-        @NotBlank(message = "El nombre es obligatorio") String name,
+        @NotBlank(message = "El nombre es obligatorio")
+        @Size(max = 100)
+        @Pattern(
+                regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 .,-]+$",
+                message = "Solo se aceptan letras, números, y guiones."
+        )
+        String name,
+
+        @Size(max = 500, message = "La descripción no puede superar los 500 caracteres.")
         String description,
-        @NotBlank(message = "La categoría es obligatoria") String category,
-        @NotNull(message = "La cantidad es obligatoria") @Min(value = 0, message = "La cantidad no puede ser negativa") Integer quantity,
+
+        @NotBlank(message = "La categoría es obligatoria")
+        @Size(max = 50, message = "La categoría no puede superar los 50 caracteres.")
+        @Pattern(
+                regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 .,-]+$",
+                message = "Solo se aceptan letras, números, y guiones."
+        )
+        String category,
+
+        @NotNull(message = "La cantidad es obligatoria")
+        @Min(value = 0, message = "La cantidad no puede ser negativa")
+        Integer quantity,
+
+        @Pattern(
+                regexp = "^(https?://).+$",
+                message = "La URL debe comenzar con http:// o https://"
+        )
         String imageUrl
 ) {}
--- src/main/java/com/acme/backendfreshsense/inventory/application/dto/UpdateProductRequest.java
+++ src/main/java/com/acme/backendfreshsense/inventory/application/dto/UpdateProductRequest.java
@@ -3,5 +3,6 @@ package com.acme.backendfreshsense.inventory.application.dto;
 import jakarta.validation.constraints.Min;
 
 public record UpdateProductRequest(
-        @Min(value = 0, message = "La cantidad no puede ser negativa") Integer quantity
+        @Min(value = 0, message = "La cantidad no puede ser negativa")
+        Integer quantity
 ) {}
